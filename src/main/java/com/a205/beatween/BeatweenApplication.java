5a3c6631badd832d2de1d4e9ff58d7a14bfb0fc0
이규리 <kllee0309@gmail.com>
2025-05-05 12:04:25 +0900

    chore: 백엔드 개발 환경 세팅

---
+++ src/main/java/com/a205/beatween/BeatweenApplication.java
@@ -0,0 +1,13 @@
+package com.a205.beatween;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatweenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatweenApplication.class, args);
+	}
+
+}
