c2fc5e74da9e0c43162ddfccbf72c89229ac244d
백지민 <eosa1414@gmail.com>
2025-05-07 06:17:10 +0900

    feat: figma의 커스텀 컬러를 tailwind theme로 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandcolor100: "#5B6FA9",
+        brandcolor200: "#68FFA4",
+        neutral100: "#FBF9F8",
+        neutral200: "#E8EBED",
+        neutral300: "#D5D9DB",
+        neutral400: "#C0C9D0",
+        neutral500: "#A6B5C0",
+        neutral600: "#8596A4",
+        neutral700: "#677681",
+        neutral800: "#324655",
+        neutral900: "#152838",
+        neutral1000: "#020923",
+        error: "#FF686B",
+        warning: "#FFCB47",
+        success: "#68FFA4",
+        "kakao-container": "#FEE500",
+        "kakao-symbol": "#000000",
+        "kakao-label": "rgba(0, 0, 0, 0.85)",
+      },
+    },
   },
   plugins: [],
-}
+};
