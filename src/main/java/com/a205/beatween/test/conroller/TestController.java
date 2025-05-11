7462773be194093c511ffec38457db09cda19184
Kyuli Lee <kllee0309@gmail.com>
2025-05-11 15:14:56 +0900

    test: push 시 자동 배포 테스트

---
+++ src/main/java/com/a205/beatween/test/conroller/TestController.java
@@ -0,0 +1,16 @@
+package com.a205.beatween.test.conroller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping(value = "/test", produces = MediaType.TEXT_HTML_VALUE)
+    public String test() {
+        return "<h1>beatween test complete!</h1>";
+    }
+}
