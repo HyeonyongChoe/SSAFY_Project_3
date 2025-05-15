9edb5ef61a2073665ef3aa541ddc4af791b2beb9
백지민 <eosa1414@gmail.com>
2025-05-15 09:48:49 +0900

    refactor: Result 클래스와 ResponseDto로 반환값을 분리하여 SPR 강화

--- src/main/java/com/a205/beatween/domain/song/controller/SongController.java
+++ src/main/java/com/a205/beatween/domain/song/controller/SongController.java
@@ -1,6 +1,7 @@
 package com.a205.beatween.domain.song.controller;
 
-import com.a205.beatween.common.reponse.ApiResponse;
+import com.a205.beatween.common.reponse.ResponseDto;
+import com.a205.beatween.common.reponse.Result;
 import com.a205.beatween.domain.song.dto.CopySheetResponseDto;
 import com.a205.beatween.domain.song.service.SongService;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +15,14 @@ public class SongController {
     private final SongService songService;
 
     @GetMapping("/{songId}/categories/{categoryId}/sheets/{sheetId}")
-    public ResponseEntity<ApiResponse<CopySheetResponseDto>> getSheet(
+    public ResponseEntity<ResponseDto<CopySheetResponseDto>> getSheet(
             @PathVariable("spaceId") Integer spaceId,
             @PathVariable("songId") Integer songId,
             @PathVariable("categoryId") Integer categoryId,
             @PathVariable("sheetId") Integer sheetId,
             @RequestHeader("X-USER-ID") Integer userId //로그인 구현 이전 임시 헤더
     ){
-        ApiResponse<CopySheetResponseDto> res = songService.getCopySheet(userId, spaceId, songId, categoryId, sheetId);
-        return ResponseEntity.ok(res);
+        Result<CopySheetResponseDto> result = songService.getCopySheet(userId, spaceId, songId, categoryId, sheetId);
+        return ResponseEntity.ok(ResponseDto.from(result));
     }
 }
--- src/main/java/com/a205/beatween/domain/song/service/SongService.java
+++ src/main/java/com/a205/beatween/domain/song/service/SongService.java
@@ -1,15 +1,13 @@
 package com.a205.beatween.domain.song.service;
 
-import com.a205.beatween.common.reponse.ApiResponse;
+import com.a205.beatween.common.reponse.ResponseDto;
+import com.a205.beatween.common.reponse.Result;
 import com.a205.beatween.domain.song.dto.CopySheetResponseDto;
 import com.a205.beatween.domain.song.entity.CopySheet;
 import com.a205.beatween.domain.song.repository.CopySheetRepository;
 import com.a205.beatween.domain.song.repository.CopySongRepository;
 import com.a205.beatween.domain.space.repository.CategoryRepository;
 import com.a205.beatween.domain.space.service.SpaceService;
-import com.a205.beatween.exception.BusinessException;
-import com.a205.beatween.exception.ErrorCode;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -23,20 +21,20 @@ public class SongService {
 
     private final SpaceService spaceService;
 
-    public ApiResponse<CopySheetResponseDto> getCopySheet(Integer userId, Integer spaceId, Integer songId, Integer categoryId, Integer sheetId){
+    public Result<CopySheetResponseDto> getCopySheet(Integer userId, Integer spaceId, Integer songId, Integer categoryId, Integer sheetId){
         boolean isMember = spaceService.checkUserIsMemberOfSpace(userId, spaceId);
         if(!isMember) {
-            return ApiResponse.error(HttpStatus.FORBIDDEN.value(), "요청한 사용자가 해당 밴드의 멤버가 아닙니다.");
+            return Result.error(HttpStatus.FORBIDDEN.value(), "요청한 사용자가 해당 밴드의 멤버가 아닙니다.");
         }
 
         boolean isCategoryInSpace = categoryRepository.existsByCategoryIdAndSpace_SpaceId(categoryId, spaceId);
         if (!isCategoryInSpace) {
-            return ApiResponse.error(HttpStatus.BAD_REQUEST.value(), "요청한 카테고리는 해당 밴드에 속하지 않습니다.");
+            return Result.error(HttpStatus.BAD_REQUEST.value(), "요청한 카테고리는 해당 밴드에 속하지 않습니다.");
         }
 
         boolean isSongInCategory = copySongRepository.existsByCopySongIdAndCategory_CategoryId(songId, categoryId);
         if (!isSongInCategory) {
-            return ApiResponse.error(HttpStatus.BAD_REQUEST.value(), "선택한 곡은 해당 카테고리에 존재하지 않습니다.");
+            return Result.error(HttpStatus.BAD_REQUEST.value(), "선택한 곡은 해당 카테고리에 존재하지 않습니다.");
         }
 
         CopySheet sheet = copySheetRepository
@@ -44,9 +42,9 @@ public class SongService {
                 .orElse(null); //값이 없다면(not found) null
 
         if (sheet == null) {
-            return ApiResponse.error(HttpStatus.NOT_FOUND.value(), "선택한 악보를 찾을 수 없습니다.");
+            return Result.error(HttpStatus.NOT_FOUND.value(), "선택한 악보를 찾을 수 없습니다.");
         }
 
-        return ApiResponse.success(CopySheetResponseDto.from(sheet));
+        return Result.success(CopySheetResponseDto.from(sheet));
     }
 }
