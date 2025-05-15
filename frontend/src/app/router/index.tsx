e95586fdb586ac9e1aa529d56176e91a492cc385
Kyuli Lee <kllee0309@gmail.com>
2025-05-15 21:15:09 +0900

    fix: 아직 없는 TeamSpacePage부분 주석 처리

--- frontend/src/app/router/index.tsx
+++ frontend/src/app/router/index.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
           },
           {
             path: "/team/:teamId",
-            element: <TeamSpacePage />,
+            // element: <TeamSpacePage />,
           },
         ],
       },
