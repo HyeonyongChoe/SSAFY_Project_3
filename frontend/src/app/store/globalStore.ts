fffe40de097bfdb998dccb7034b3d40406002f22
seo <jjkkgh123@gmail.com>
2025-05-15 14:06:44 +0900

    WIP: globalStore 변경사항 저장

--- frontend/src/app/store/globalStore.ts
+++ frontend/src/app/store/globalStore.ts
@@ -1,12 +1,18 @@
 // src/app/store/globalStore.ts
-import { create } from 'zustand';
+import { create } from "zustand";
 
 type GlobalState = {
   isLoggedIn: boolean;
   setIsLoggedIn: (status: boolean) => void;
+
+  isPlaying: boolean;
+  setIsPlaying: (status: boolean) => void;
 };
 
 export const useGlobalStore = create<GlobalState>((set) => ({
   isLoggedIn: false,
   setIsLoggedIn: (status) => set({ isLoggedIn: status }),
+
+  isPlaying: false,
+  setIsPlaying: (status) => set({ isPlaying: status }),
 }));
