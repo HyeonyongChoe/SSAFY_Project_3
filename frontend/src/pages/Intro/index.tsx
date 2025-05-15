af94bfa889fd34b00cb0f5b91b90ff2285e84a6a
Kyuli Lee <kllee0309@gmail.com>
2025-05-15 16:22:37 +0900

    test

--- frontend/src/pages/Intro/index.tsx
+++ frontend/src/pages/Intro/index.tsx
@@ -30,7 +30,9 @@ export const IntroPage = ({ onFinish }: IntroPageProps) => {
           exit={{ opacity: 0 }}
           transition={{ duration: 1 }}
         >
-          <div className="text-neutral100">박자로 채우는 우리 사이</div>
+          <div className="text-neutral100">
+            박자로 채우는 우리 사이????????????
+          </div>
         </motion.div>
       )}
     </AnimatePresence>
