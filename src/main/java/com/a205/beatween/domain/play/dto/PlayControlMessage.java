c38b65d77b39d14251c394712ca7add9d2aa738a
백지민 <eosa1414@gmail.com>
2025-05-16 12:13:56 +0900

    chore: 필드 변경

--- src/main/java/com/a205/beatween/domain/play/dto/PlayControlMessage.java
+++ src/main/java/com/a205/beatween/domain/play/dto/PlayControlMessage.java
@@ -14,8 +14,9 @@ import java.util.Map;
 public class PlayControlMessage {
     private Integer spaceId;
     private int bpm;
-    private long startTimestamp;
+    private long startTimestamp; // 시작 시각
     private boolean playing;
-    private Map<Integer, Double> measureMap;
+    private int currentMeasure; //현재 마디
+    private double positionInMeasure; // 마디에서의 경과 시간
     private Integer sender; // userId
 }
