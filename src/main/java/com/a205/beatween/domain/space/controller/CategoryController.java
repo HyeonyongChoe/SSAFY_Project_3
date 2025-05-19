b1b623ba80b5da1c775031807bd87dbdf1cd8d7c
최현용 <gusdyddl1212@naver.com>
2025-05-19 19:24:14 +0900

    feat: 카테고리 컨트롤러 생성

---
+++ src/main/java/com/a205/beatween/domain/space/controller/CategoryController.java
@@ -0,0 +1,15 @@
+package com.a205.beatween.domain.space.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/spaces/{spaceId}/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+//    @PostMapping("/")
+
+}
