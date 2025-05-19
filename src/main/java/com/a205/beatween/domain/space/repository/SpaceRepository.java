3d86d273d764b98af37836a2fd81eb7bd5908afa
최현용 <gusdyddl1212@naver.com>
2025-05-19 19:23:47 +0900

    feat: 유저의 space 리스트 조회

--- src/main/java/com/a205/beatween/domain/space/repository/SpaceRepository.java
+++ src/main/java/com/a205/beatween/domain/space/repository/SpaceRepository.java
@@ -1,9 +1,20 @@
 package com.a205.beatween.domain.space.repository;
 
+import com.a205.beatween.domain.space.dto.SpacePreDto;
 import com.a205.beatween.domain.space.entity.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SpaceRepository extends JpaRepository<Space, Integer> {
+
+    @Query("SELECT new com.a205.beatween.domain.space.dto.SpacePreDto(" +
+            "s.spaceId, s.name, s.imageUrl, s.spaceType) " +
+            "FROM Space as s " +
+            "JOIN UserSpace us ON s.spaceId = us.space.spaceId " +
+            "WHERE us.user.userId =:userId")
+    List<SpacePreDto> findByUserId(Integer userId);
 }
