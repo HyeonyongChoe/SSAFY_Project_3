43512a7ebb868a7f3620861c556a8b76905d9f89
LeeSeongHui <seonghui3472@gmail.com>
2025-05-22 03:31:11 +0900

    fix : jwtUtil error solve

--- src/main/java/com/a205/beatween/common/config/WebSocketConfig.java
+++ src/main/java/com/a205/beatween/common/config/WebSocketConfig.java
@@ -2,26 +2,22 @@ package com.a205.beatween.common.config;
 
 import com.a205.beatween.common.event.CustomHandshakeHandler;
 import com.a205.beatween.common.event.UserHandshakeInterceptor;
+import com.a205.beatween.common.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.XSlf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.messaging.SessionConnectEvent;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    private static final AtomicInteger connectionCount = new AtomicInteger(0);
+    private final JwtUtil jwtUtil;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -35,7 +31,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .setHandshakeHandler(new CustomHandshakeHandler())
-                .addInterceptors(new UserHandshakeInterceptor())
+                .addInterceptors(new UserHandshakeInterceptor(jwtUtil))
                 .withSockJS();
     }
 
