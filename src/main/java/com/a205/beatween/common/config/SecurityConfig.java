75e9d01e49f7cd03f8f72cefd3b1a0142484c984
LeeSeongHui <seonghui3472@gmail.com>
2025-05-22 05:25:18 +0900

    fix : cors modify3

--- src/main/java/com/a205/beatween/common/config/SecurityConfig.java
+++ src/main/java/com/a205/beatween/common/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.a205.beatween.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,14 +17,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll() // 모든 요청 허용 (개발 단계)
-                )
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())); // 새로운 방식으로 CORS 설정
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/**").permitAll()
+                );
 
         return http.build();
     }
+
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
