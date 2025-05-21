f297b787b565b4fdf6621ba5087d7553356327d3
seo <jjkkgh123@gmail.com>
2025-05-21 11:27:56 +0900

    feat: 밴드 생성

--- frontend/src/features/createBand/api/createBandApi.ts
+++ frontend/src/features/createBand/api/createBandApi.ts
@@ -12,15 +12,9 @@ export const createBandApi = async (
   if (data.description) formData.append("description", data.description);
   if (data.image) formData.append("image", data.image); // optional
 
-  const headers = {
-    "X-USER-ID": "1", // ✅ 임시 유저 ID
-    // ✅ Content-Type 생략 (브라우저가 multipart 설정함)
-  };
-
   const response = await axiosInstance.post<CreateBandResponse>(
     "/api/v1/spaces",
-    formData,
-    { headers }
+    formData
   );
 
   return response.data;
