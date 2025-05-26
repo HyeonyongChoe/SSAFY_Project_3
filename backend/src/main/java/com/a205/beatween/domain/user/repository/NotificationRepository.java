8179664e21cb1d497798a4501ee8a02dfe448ee7
최현용 <gusdyddl1212@naver.com>
2025-05-26 14:38:16 +0900

    feat: 알림, 유저_알림 Entity, Repository 작성 작성

---
+++ backend/src/main/java/com/a205/beatween/domain/user/repository/NotificationRepository.java
@@ -0,0 +1,24 @@
+package com.a205.beatween.domain.user.repository;
+
+import com.a205.beatween.domain.user.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+
+    @Query("SELECT new com.a205.beatween.domain.user.dto.NotificationDto(" +
+            "un.userNotificationId, n.notificationId, " +
+            "n.space.spaceId, n.type, n.content, " +
+            "n.createdAt, un.isRead) " +
+            "FROM Notification n " +
+            "JOIN UserNotification un " +
+            "ON n.notificationId = un.notification.notificationId " +
+            "WHERE un.user.userId =: userId " +
+            "AND un.isRead = false " +
+            "ORDER BY n.createdAt DESC ")
+    List<Notification> findByUserId(Integer userId);
+}
